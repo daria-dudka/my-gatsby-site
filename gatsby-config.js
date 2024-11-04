dcd9d5c7c5422a601b31ab83dae47e4dddc31ca6
Daria Dudka <66278954+daria-dudka@users.noreply.github.com>
2024-11-04 16:55:01 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
+    title: `My First Gatsby Site`,
     siteUrl: `https://mygatsbysitedudka.netlify.app`,
   },
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
